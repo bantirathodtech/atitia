ceba1998d33b64339e265c89a0f20278abfba7f6
bantirathodtech <bantirathodtech@gmail.com>
2025-11-24 11:43:59 +0530

    chore: Add index export files for easier imports

---
+++ lib/common/widgets/indicators/index.dart
@@ -0,0 +1,7 @@
+// lib/common/widgets/indicators/index.dart
+
+/// Empty state and indicator widgets with consistent styling
+export 'empty_state.dart';
+export 'enhanced_empty_state.dart';
+export 'step_indicator.dart';
+
---
+++ lib/common/widgets/loaders/index.dart
@@ -0,0 +1,7 @@
+// lib/common/widgets/loaders/index.dart
+
+/// Loading state widgets with shimmer effects and skeleton screens
+export 'adaptive_loader.dart';
+export 'shimmer_loader.dart';
+export 'enhanced_loading_state.dart';
+
